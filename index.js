c0dd20e5109471f30296bd1349790f40fc00f3d8
TardisKing40 <tardisking40@gmail.com>
2026-06-03 21:06:20 +1000

    dont require extensions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 
-app.use(express.static('public'))
+app.use(express.static('public', {extensions:['html']}));
 app.use('/.well-known', express.static('.well-known'));
 
 app.use((req, res, next) => {
