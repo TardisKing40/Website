931ed3cd3f5f501b1b11f2d1c92dbe8732a06b88
TardisKing40 <tardisking40@gmail.com>
2026-06-03 23:28:24 +1000

    better 404

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 
-
 app.use(express.static('public', {extensions:['html']}));
 app.use('/.well-known', express.static('.well-known'));
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + '/public/404.html');
+    res.status(404).send("<body><img src='https://http.cat/404' alt='404 Not Found'></body>")
 })
 
 app.listen(3000, () => {
